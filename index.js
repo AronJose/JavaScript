7442f0c7735d40572086f30b16fa42c56a7e1984
AronJose <aronjose@gmail.com>
2023-05-22 22:02:48 +0530

    Methoda Array,String,Number

--- index.js
+++ index.js
@@ -45,3 +45,37 @@ myFuctin = (c) =>{
      */
     let myArray =["test",25,"value"]
 
+// String Method
+    let a="Aron Jose e" ;
+      /*"a" enna variable 'Aron Jose' enna String eduth athinde length edukkan .
+      "variablename.length" (Eg:a.length).
+      a.indexOf() use cheyunnath eth position ulla characher position find out cheyan ann.
+      */
+     console.log(a.length);
+     console.log(a.indexOf(J));
+     console/log(a.lastIndexOf(e)); //last ezhuthiya characher edukku like "e".
+     let b = a.slice(0,6);//ith pole ellam ezhutham like length,indexOf,LastindexOf,search,include..
+     console.log(b);//slice use cheyth index 0 to 6 word print cheyan.
+
+// Number Method
+    let aa = "15";
+    let bb = 15;
+    console.log(parseInt(aa));//"a" string ann set akki ekkunnath athinde integer akkunnu
+     /*
+     "parseInt(aa)" : String to integer
+     "bb.toString()" :Integer to String
+      */
+
+// Array Methods
+    let Array=["a","b","c","d"];
+    Array[0]="g";//Array Update ,0 index value mari g akum.
+    Array.push("H");//"push()" enna method use cheyth array new vvalue add cheyunne.
+    Array.unshift("H");//Array ettavum first pppostion "H" varum.
+    Array.pop();//last position value will removed,No value need to remove.
+    Array.shift();//Fist value will removed.
+    Array.splice(0,1);//nammak istam ulla position value remove cheyan index value 0 to 1.
+    Array.splice(2,0,"r","t");//index 2 is the value postion where to add and 0 indicate that not delete value,then add values
+   let newArray = Array.slice();//slice function new array creat cheyth athil value store cheyum.
+    //slice use cheyunnath kond index vecha store cheyunne ,So once value onn vende "slice(1)" first index thot stor akum.
+    Array.sort();//Array sort cheyam.
+    Array.reverse();//Array reverse cheyth ezhutham.
