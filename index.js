762eb6cce6752f96c6afa943e12e3d272d5f8f32
AronJose <aronjose@gmail.com>
2023-05-22 09:19:27 +0530

    Function

---
+++ index.js
@@ -0,0 +1,17 @@
+//Functions
+
+ //Bassic Function Syntax
+      function myFunc(){
+            let a=5;
+            return a;
+      }
+      myFunc();//ithan function call cheythekkunnath
+
+// FUnction with parameter
+      function myFunction(z){
+            let  a=5;
+            let b=z;
+            let c=a+b;
+            return c;
+      }
+      myFunction(10);
