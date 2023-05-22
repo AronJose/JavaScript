e2ea58a2f649397dc53f6dbb2bda8dec35935b90
AronJose <aronjose@gmail.com>
2023-05-22 11:36:10 +0530

    Objects and Array

--- index.js
+++ index.js
@@ -15,3 +15,34 @@
             return c;
       }
       myFunction(10);
+
+// Arrow Function 
+ /* Now a day most poppulat is Arrow function 
+ function name = (parameter) => retuen parameter
+ */
+myFuctin = (c) =>{
+      return c;
+}
+
+// Objects
+   /*
+    Object main ayyt use cheyunnath variables nammal oronnum perukki ann
+    ezhuthunnath athin motham oru block ayyt ezhuthunnath ann objects,
+    for exaple user details full orumich onnil set akkam,
+    object use cheythal at a time full display cheyam loop use cheyth ,
+    object call cheyunnath " object name.variable name " or
+    allel motham kittan oject name mathram kodutha maathy
+    */
+    let myObject={
+      name :"aron",
+      DOB :"19-06-98"
+    };//let objectname ={ variablename:"value",variablename:"value",};
+
+
+// Aarray in JavaScript
+    /*
+    Array not a key and value paire it is indexx based to store tha vlaues
+     */
+    let myArray =["test",25,"value"]
+
+    
