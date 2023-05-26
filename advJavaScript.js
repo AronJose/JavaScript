6363f5e5a4dce8b3358099b4bb56a020de9531ce
AronJose <aronjose@gmail.com>
2023-05-26 18:36:28 +0530

    Methods

--- advJavaScript.js
+++ advJavaScript.js
@@ -82,3 +82,57 @@
       const newfilterarray=object1.filter((value)=>value.age===20 && value.id===1);//20 age ullath mathram print cheyum
             //athpole multiple values check cheyan sathikkum.
       console.log(newfilterarray);
+
+// Reduce Method
+      /* Reduce method main ayyt use cheyunnath oru array values add cheyunnathinn okke ann */
+      const addArray=[10,15,20,25,30];
+     // Eg:
+            let total=0;
+            for(let i=0;i<addArray.length;i++){
+                  document.write(total[i]);
+            }//ith ann normal method But JavaScript advace ayyt ithine minimise cheyth Reduce method use cheyunnu.
+
+      const newAddArray=addArray.reduce((total,value,index,array)=>total+value,0);//"total" variable ann reduce method
+      //4 parameter carry cheyum values,index,full array and a variable.
+
+
+//Find Method
+      /* 
+      Find method main ayyt use cheyunnath oru 
+      example vech paranjal oru array of object und athil onn mathram eduth print cheyanam  
+      vere onnum venda  athinn find methos use cheyanm
+      */
+      const object2=[
+            {
+                  id:0,
+                  name:"aron",
+                  age:20
+            },
+            {
+                  id:1,
+                  name:"gisna",
+                  age:25           
+            },
+            {
+                  id:2,
+                  name:"gisna1",
+                  age:20           
+            },
+      ];
+      const findobject=object2.find((value)=>value.name==="aron");
+
+// Sort method
+const sortArray=[10,15,20,5,25,30];
+sortArray.sort();//evide sort first kidakunnathe nokku 10nth position nokkulla soo sort currect akulla
+console.log(sortArray);
+const arraysort=sortArray.sort((a,b)=>a-b);//"a"first position and "b" is the second position this is the accesinding order
+const arraysort1=sortArray.sort((a,b)=>b-a);//decending order.
+
+
+//High Order method ellam chain cheyam
+const newArrayobject1=object1.map((value)=>value.name).filter((value)=>value.age===20).reduce((total,value)=>total+value,0);
+
+
+// Flate Array concept
+      /* A single array conttains nest array so we use flate()method */
+      arrayName.flat(Infinity);//"infinity" enna parameter kodutha ethra nest array undelum set ayy single ara ayy kanikkum. 
